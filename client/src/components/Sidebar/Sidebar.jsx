531a57d74dfb4ff3bb4aeebce6e80b4090204c3f
tahseen25 <bairagdartahseen@gmail.com>
2024-12-19 21:50:47 +0530

    Create Sidebar.jsx

---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+// src/components/Sidebar/Sidebar.jsx
+
+import React, { useState } from "react";
+import plusIcon from "../../assets/plus.svg";
+import "./Sidebar.css";
+
+function Sidebar({ addNote }) {
+  const colors = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff","#a0c4ff"];
+  const [listOpen, setListOpen] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <img src={plusIcon} alt="Add" onClick={() => setListOpen(!listOpen)} />
+      <ul className={`sidebar_list ${listOpen ? "sidebar_list_active" : ""}`}>
+        {colors.map((color, index) => (
+          <li
+            key={index}
+            className="sidebar_list_item"
+            style={{ backgroundColor: color }}
+            onClick={() => addNote(color)}  // Pass the correct color
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
