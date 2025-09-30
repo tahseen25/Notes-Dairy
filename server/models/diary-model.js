cbb98dc1fbfae59844a0461c4628e208830cdc38
Tahseen Abdulrahim Bairagdar <bairagdartahseen@gmail.com>
2025-09-30 14:01:38 +0530

    Update to encrpt dialy enrty

--- server/models/diary-model.js
+++ server/models/diary-model.js
@@ -1,30 +1,41 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const Schema = mongoose.Schema;
 
 const diarySchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    entry: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the user who owns the diary entry
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    color: {
-        type: String ,// Optional field
-        default: '#FFFFFF'
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  entry: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  color: {
+    type: String,
+    default: '#FFFFFF'
+  }
+});
+
+// Load encryption keys from environment variables
+const encKey = process.env.ENC_KEY;
+const sigKey = process.env.SIG_KEY;
+
+// Apply encryption to sensitive fields
+diarySchema.plugin(encrypt, {
+  encryptionKey: encKey,
+  signingKey: sigKey,
+  encryptedFields: ["title", "entry"]
 });
 
 const Diary = mongoose.model('Diary', diarySchema);
-
 module.exports = Diary;
