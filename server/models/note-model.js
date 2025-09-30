f59be8f2df3f46b4301bf081f3378bfa78df42dc
Tahseen Abdulrahim Bairagdar <bairagdartahseen@gmail.com>
2025-09-30 14:02:49 +0530

    Update to encrypt notes entry
    run npm install mongoose-encryption

--- server/models/note-model.js
+++ server/models/note-model.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-    title: {
-        type: String
-    },
-    
-    body: {
-        type: String,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    color: {
-        type: String // Optional field
-    }
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  color: {
+    type: String
+  }
+});
+
+// Load encryption keys from environment variables
+const encKey = process.env.ENC_KEY; // 32 bytes
+const sigKey = process.env.SIG_KEY; // 64 bytes
+
+// Apply encryption to sensitive fields
+noteSchema.plugin(encrypt, {
+  encryptionKey: encKey,
+  signingKey: sigKey,
+  encryptedFields: ["title", "body"]
 });
 
 const Note = mongoose.model('Note', noteSchema);
-
 module.exports = Note;
