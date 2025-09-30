e3cd7e68f9392d93b4106f8fba304c95300b16f9
Tahseen Abdulrahim Bairagdar <bairagdartahseen@gmail.com>
2025-09-30 15:18:14 +0530

    Update diary-model.js
    to generate seperate keys for each user

--- server/models/diary-model.js
+++ server/models/diary-model.js
@@ -1,41 +1,46 @@
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
 const Schema = mongoose.Schema;
+const { encrypt, decrypt } = require('../utils/encryption');
 
 const diarySchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  entry: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  color: {
-    type: String,
-    default: '#FFFFFF'
-  }
+    title: {
+        type: String
+    },
+    entry: {
+        type: String
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    color: {
+        type: String,
+        default: '#FFFFFF'
+    }
 });
 
-// Load encryption keys from environment variables
-const encKey = process.env.ENC_KEY;
-const sigKey = process.env.SIG_KEY;
-
-// Apply encryption to sensitive fields
-diarySchema.plugin(encrypt, {
-  encryptionKey: encKey,
-  signingKey: sigKey,
-  encryptedFields: ["title", "entry"]
+// Encrypt before saving
+diarySchema.pre('save', function(next) {
+    if (this.isModified('title') && this.title && this.userKey) {
+        this.title = encrypt(this.title, this.userKey);
+    }
+    if (this.isModified('entry') && this.entry && this.userKey) {
+        this.entry = encrypt(this.entry, this.userKey);
+    }
+    next();
 });
+
+// Decrypt after fetching
+diarySchema.methods.decryptFields = function(userKey) {
+    if (this.title) this.title = decrypt(this.title, userKey);
+    if (this.entry) this.entry = decrypt(this.entry, userKey);
+};
 
 const Diary = mongoose.model('Diary', diarySchema);
+
 module.exports = Diary;
